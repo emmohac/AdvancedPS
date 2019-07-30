01b56a797131b3772c031ed268454a76dac84184
Tran <Huy.Tran@unisys.com>
2019-07-30 15:12:43 -0700

    Hover and color

--- support.js
+++ support.js
@@ -1,3 +1,23 @@
+console.log("If you see this message then I believe you are not a normal visitor of my portfolio.");
+console.log("You are either a recruiter or someone who is looking to learn about basic frontend design (normal people would not inspect the website).");
+console.log("If you are a recruiter and think that there is a position that fits my skills, please contact me by email.");
+console.log("If you are someone who is looking to build basic frontend and have question, please contact me by email and I will try to help as much as possible.");
+
+$("#uci").hover(function () {
+    $(this).css("background-color", "#00386c");
+    $(this).css("color", "#f6aa0d");
+}, function () {
+    $(this).css("background-color", "#343a40");
+    $(this).css("color", "whitesmoke");
+});
+
+$("#cs").hover(function () {
+    $(this).css("background-color", "#333");
+    $(this).css("color", "#00cc00")
+}, function () {
+    $(this).css("background-color", "#343a40");
+    $(this).css("color", "whitesmoke");
+});
 $("#year").text(new Date().getFullYear());
 
 $("body").scrollspy({
